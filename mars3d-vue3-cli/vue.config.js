9068dc715ae071dba98a73c9d9d1981140f62d62
wanghao <muyao1987@qq.com>
2021-11-24 10:21:24 +0800

    修改cesiumRunPath

--- mars3d-vue3-cli/vue.config.js
+++ mars3d-vue3-cli/vue.config.js
@@ -14,36 +14,20 @@ module.exports = {
   assetsDir: 'static',
   productionSourceMap: false,
   configureWebpack: (config) => {
-    let plugins = []
-    if (process.env.NODE_ENV === 'production') {
-      plugins = [
-        new webpack.DefinePlugin({
-          CESIUM_BASE_URL: JSON.stringify('./static')
-        }),
-        new CopyWebpackPlugin({
-          patterns: [
-            { from: path.join(cesiumSource, 'Workers'), to: './static/Workers' },
-            { from: path.join(cesiumSource, 'Assets'), to: './static/Assets' },
-            { from: path.join(cesiumSource, 'ThirdParty'), to: './static/ThirdParty' },
-            { from: path.join(cesiumSource, 'Widgets'), to: './static/Widgets' }
-          ]
-        })
-      ]
-    } else {
-      plugins = [
-        new webpack.DefinePlugin({
-          CESIUM_BASE_URL: JSON.stringify('./')
-        }),
-        new CopyWebpackPlugin({
-          patterns: [
-            { from: path.join(cesiumSource, 'Workers'), to: './Workers' },
-            { from: path.join(cesiumSource, 'Assets'), to: './Assets' },
-            { from: path.join(cesiumSource, 'ThirdParty'), to: './ThirdParty' },
-            { from: path.join(cesiumSource, 'Widgets'), to: './Widgets' }
-          ]
-        })
-      ]
-    }
+
+    let cesiumRunPath = config.output.publicPat || './cesium/' //cesium运行时主目录
+    let plugins = [
+      //标识cesium资源所在的主目录，cesium内部资源加载、多线程等处理时需要用到
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify(cesiumRunPath)
+      }),
+      //cesium相关资源目录需要拷贝到系统目录下面
+      new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Workers'), to: cesiumRunPath + 'Workers' }]),
+      new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Assets'), to: cesiumRunPath + 'Assets' }]),
+      new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'ThirdParty'), to: cesiumRunPath + 'ThirdParty' }]),
+      new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Widgets'), to: cesiumRunPath + 'Widgets' }])
+    ]
+
     config.plugins = [
       ...config.plugins,
       ...plugins
