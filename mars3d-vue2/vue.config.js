45a727c7f3a06df39b24fa811fc0fe769f97db9b
wanghao <muyao1987@qq.com>
2022-04-18 16:32:32 +0800

    修改module

--- mars3d-vue2/vue.config.js
+++ mars3d-vue2/vue.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-
 module.exports = {
   publicPath: './',
   assetsDir: './static',
@@ -16,34 +15,30 @@ module.exports = {
     open: true // 配置自动启动浏览器
   },
   configureWebpack: (config) => {
-
-
-    // let cesiumSourcePath = 'node_modules/mars3d-cesium/Build/Cesium/' //cesium库目录
-    // let cesiumRunPath = config.output.publicPath || './cesium/' //cesium运行时主目录
-    let plugins = [
-      //标识cesium资源所在的主目录，cesium内部资源加载、多线程等处理时需要用到
-      // new webpack.DefinePlugin({
-      //   CESIUM_BASE_URL: JSON.stringify(cesiumRunPath)
-      // }),
-      // //cesium相关资源目录需要拷贝到系统目录下面
-      // new CopyWebpackPlugin([{ from: path.join(cesiumSourcePath, 'Workers'), to: path.join(config.output.path, cesiumRunPath, 'Workers') }]),
-      // new CopyWebpackPlugin([{ from: path.join(cesiumSourcePath, 'Assets'), to: path.join(config.output.path, cesiumRunPath, 'Assets') }]),
-      // new CopyWebpackPlugin([{ from: path.join(cesiumSourcePath, 'ThirdParty'), to: path.join(config.output.path, cesiumRunPath, 'ThirdParty') }]),
-      // new CopyWebpackPlugin([{ from: path.join(cesiumSourcePath, 'Widgets'), to: path.join(config.output.path, cesiumRunPath, 'Widgets') }])
-    ]
-
+    let plugins = []
+    if (process.env.VUE_APP_MARS3D_SOURCE === 'module') {
+      const cesiumSourcePath = 'node_modules/mars3d-cesium/Build/Cesium/' // cesium库目录
+      const cesiumRunPath = config.output.publicPath || './cesium/' // cesium运行时主目录
+      plugins = [
+      // 标识cesium资源所在的主目录，cesium内部资源加载、多线程等处理时需要用到
+        new webpack.DefinePlugin({
+          CESIUM_BASE_URL: JSON.stringify(cesiumRunPath)
+        }),
+        // cesium相关资源目录需要拷贝到系统目录下面
+        new CopyWebpackPlugin({
+          patterns: [
+            { from: path.join(cesiumSourcePath, 'Workers'), to: path.join(config.output.path, cesiumRunPath, 'Workers') },
+            { from: path.join(cesiumSourcePath, 'Assets'), to: path.join(config.output.path, cesiumRunPath, 'Assets') },
+            { from: path.join(cesiumSourcePath, 'ThirdParty'), to: path.join(config.output.path, cesiumRunPath, 'ThirdParty') },
+            { from: path.join(cesiumSourcePath, 'Widgets'), to: path.join(config.output.path, cesiumRunPath, 'Widgets') }
+          ]
+        })
+      ]
+    }
 
     return {
       module: {
-        unknownContextCritical: false,
-        rules: [
-          {
-            test: /\.js$/,
-            enforce: 'pre',
-            include: path.resolve(__dirname, 'node_modules/mars3d-cesium/Source'),
-            sideEffects: false
-          }
-        ]
+        unknownContextCritical: false
       },
       optimization: {
         usedExports: true,
@@ -60,13 +55,7 @@ module.exports = {
                 const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
                 return `npm.${packageName.replace('@', '')}`
               }
-            },
-            // commons: {
-            //   name: 'Cesium',
-            //   test: /[\\/]node_modules[\\/]mars3d-cesium[\\/]Build[\\/]Cesium/,
-            //   priority: 10,
-            //   chunks: 'all'
-            // }
+            }
           }
         }
       },
