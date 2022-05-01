f328d5d4deee7e9754c051d3158215b414feab2c
wanghao <wh@marsgis.cn>
2022-05-01 19:11:37 +0800

    1

--- mars3d-vue2/src/components/mars-work/mars-map.vue
+++ mars3d-vue2/src/components/mars-work/mars-map.vue
@@ -5,6 +5,9 @@
 <script>
 import Vue from 'vue'
 
+// import "mars3d-cesium/Build/Cesium/Widgets/widgets.css"
+// import * as Cesium from 'mars3d-cesium'
+
 // 使用免费开源版本
 import 'mars3d/dist/mars3d.css'
 import * as mars3d from 'mars3d'
