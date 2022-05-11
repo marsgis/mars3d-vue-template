85114fd97b99d2d54dc463163e45f1b4598e8a8a
wanghao <muyao1987@qq.com>
2022-05-11 13:24:00 +0800

    优化配置

--- mars3d-vue2/vue.config.js
+++ mars3d-vue2/vue.config.js
@@ -3,28 +3,30 @@ const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  publicPath: './',
-  assetsDir: './static',
-  productionSourceMap: false,
+  publicPath: '/',
+  assetsDir: 'static',
+  outputDir: 'dist',
   lintOnSave: true, // 是否开启eslint
+  productionSourceMap: false, // 不需要生产环境的 source map
+  filenameHashing: true, // 文件名哈希
   // 它支持webPack-dev-server的所有选项
   devServer: {
     host: 'localhost', // 也可以直接写IP地址这样方便真机测试
     port: 3001, // 端口号
-    https: false, // https:{type:Boolean}
     open: true // 配置自动启动浏览器
   },
   configureWebpack: (config) => {
     let plugins = []
     if (process.env.VUE_APP_MARS3D_SOURCE === 'module') {
-      const cesiumSourcePath = 'node_modules/mars3d-cesium/Build/Cesium/' // cesium库目录
-      const cesiumRunPath = config.output.publicPath || './cesium/' // cesium运行时主目录
+      const cesiumSourcePath = 'node_modules/mars3d-cesium/Build/Cesium/' // cesium库安装目录
+      const cesiumRunPath = './mars3d-cesium/' // cesium运行时路径
+
       plugins = [
-      // 标识cesium资源所在的主目录，cesium内部资源加载、多线程等处理时需要用到
+        // 标识cesium资源所在的主目录，cesium内部资源加载、多线程等处理时需要用到
         new webpack.DefinePlugin({
-          CESIUM_BASE_URL: JSON.stringify(cesiumRunPath)
+          CESIUM_BASE_URL: JSON.stringify(path.join(config.output.publicPath, cesiumRunPath))
         }),
-        // cesium相关资源目录需要拷贝到系统目录下面
+        // Cesium相关资源目录需要拷贝到系统目录下面（部分CopyWebpackPlugin版本的语法可能没有patterns）
         new CopyWebpackPlugin({
           patterns: [
             { from: path.join(cesiumSourcePath, 'Workers'), to: path.join(config.output.path, cesiumRunPath, 'Workers') },
@@ -37,47 +39,7 @@ module.exports = {
     }
 
     return {
-      module: {
-        unknownContextCritical: false
-      },
-      optimization: {
-        usedExports: true,
-        splitChunks: {
-          maxInitialRequests: Infinity,
-          minSize: 0,
-          maxSize: 250000,
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              chunks: 'all',
-              name(module) {
-                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-                return `npm.${packageName.replace('@', '')}`
-              }
-            }
-          }
-        }
-      },
-      output: {
-        sourcePrefix: ' '
-      },
-      amd: {
-        toUrlUndefined: true
-      },
-      resolve: {
-        alias: {
-          vue$: 'vue/dist/vue.esm.js',
-          '@': path.resolve('src')
-        }
-      },
-      node: {
-        fs: 'empty',
-        Buffer: false,
-        http: 'empty',
-        https: 'empty',
-        zlib: 'empty'
-      },
+      module: { unknownContextCritical: false }, // 配置加载的模块类型，cesium时必须配置
       plugins: plugins
     }
   }
--- mars3d-vue3-js/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue"
-import Application from "./App.vue"
-import "./styles/index.less" // 加载全局样式
-
-const app = createApp(Application)
-
-app.mount("#app")
