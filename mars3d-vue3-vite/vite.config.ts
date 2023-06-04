ea8099ba4afffba5e0fb5022d44051fe12ba5f20
wanghao <wh@marsgis.cn>
2023-06-04 13:42:01 +0800

    同步代码 v3.5.10

--- mars3d-vue3-vite/vite.config.ts
+++ mars3d-vue3-vite/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { vitePluginMars3d } from "vite-plugin-mars3d"
+import { mars3dPlugin } from "vite-plugin-mars3d"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vitePluginMars3d(),
+    mars3dPlugin(),
   ],
 });
